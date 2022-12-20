07078af8f681e06b536f5c1420352a7e802d7b32
Sandushi Rathnayaka <108363078+Sandushi97@users.noreply.github.com>
2022-12-20 15:10:46 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,27 @@
+#Importing the relevant libraries
+from tkinter import Label,Tk
+import time
+#Defining the size and the title of the application
+app_window=Tk()
+app_window.title('Digital Clock')
+app_window.geometry("500x300") 
+app_window.resizable(1,1)
+app_window.eval('tk::PlaceWindow . center')
+
+#font of the window and its color
+text_font=("Times New Roman",68,"italic bold")
+background="black"
+foreground= "red"
+border_width = 100
+
+#combining all the elements to defining the label of the clock application
+label=Label(app_window,font=text_font,bg=background,fg=foreground,bd=border_width)
+label.grid(row=0,column=1)
+
+
+def digital_clock(): 
+   time_live = time.strftime("%H:%M:%S")
+   label.config(text=time_live) 
+   label.after(200, digital_clock)
+digital_clock()
+#app_window.mainloop()
